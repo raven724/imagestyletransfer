ca53ec78995bcddfbe753b209adacdda5d9522b0
raven724 <kangwogus3@gmail.com>
2021-01-13 17:57:26 +0900

    0113 finished. Link all activities, initial design complete. Next popup

--- src/pri/app/src/main/java/com/pri/practice/MainPage.kt
+++ src/pri/app/src/main/java/com/pri/practice/MainPage.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import java.lang.IndexOutOfBoundsException
 
 class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
-        val btnSubpage1: Button = findViewById(R.id.btn_sub1)
-        val btnSubpage2: Button = findViewById(R.id.btn_sub2)
+        val btnSubpage1: Button = findViewById(R.id.btn_main_sub1)
+        val btnSubpage2: Button = findViewById(R.id.btn_main_sub2)
 
         btnSubpage1.setOnClickListener {
             val intentSubpage1 = Intent(this, SubActivity1::class.java)
