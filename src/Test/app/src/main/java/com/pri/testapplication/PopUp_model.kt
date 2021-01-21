22ccbe325521925176d2cada93625b69bc110b58
raven724 <kangwogus3@gmail.com>
2021-01-21 21:49:32 +0900

    0121 commit, finalize essential parts of model selection

--- src/Test/app/src/main/java/com/pri/testapplication/PopUp_model.kt
+++ src/Test/app/src/main/java/com/pri/testapplication/PopUp_model.kt
@@ -1,11 +1,31 @@
 package com.pri.testapplication
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
 
 class PopUp_model : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pop_up_model)
+
+        val radioModel: RadioGroup = findViewById(R.id.radioGroupModel)
+        val btnSelectModel: Button = findViewById(R.id.btn_modelSelect)
+
+        btnSelectModel.setOnClickListener {
+            val returnIntent = Intent()
+            var returnRadioText = String()
+            when(radioModel.checkedRadioButtonId){
+                R.id.radioModel1 -> {returnRadioText = "Model1"}
+                R.id.radioModel2 -> {returnRadioText = "Model2"}
+                R.id.radioModel3 -> {returnRadioText = "Model3"}
+            }
+            returnIntent.putExtra("returnValue", returnRadioText)
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
+        }
     }
 }
--- src/Test/app/src/main/java/com/pri/testapplication/SubPage11.kt
+++ src/Test/app/src/main/java/com/pri/testapplication/SubPage11.kt
@@ -1,5 +1,6 @@
 package com.pri.testapplication
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,22 +8,34 @@ import android.widget.Button
 import android.widget.ImageView
 
 class SubPage11 : AppCompatActivity() {
+    var modelName = "undefined"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub_page11)
 
-        val viewInputImage: ImageView = findViewById(R.id.imageInput)
+        // val viewInputImage: ImageView = findViewById(R.id.imageInput)
         val btnSelectStyle: Button = findViewById(R.id.btn_modelSelect)
         val btnSub11Sub12: Button = findViewById(R.id.btn_Sub1112)
 
         btnSelectStyle.setOnClickListener {
-            val intentSubPopup: Intent = Intent(this, PopUp_model::class.java)
-            startActivity(intentSubPopup)
+            val intentSubPopup = Intent(this, PopUp_model::class.java)
+            startActivityForResult(intentSubPopup, 50)
         }
 
         btnSub11Sub12.setOnClickListener {
-            val intentSub11Sub12: Intent = Intent(this, SubPage12::class.java)
+            val intentSub11Sub12 = Intent(this, SubPage12::class.java)
+            intentSub11Sub12.putExtra("modelType", modelName)
             startActivity(intentSub11Sub12)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            when(requestCode){
+                50 -> modelName = data?.getStringExtra("returnValue").toString()
+            }
         }
     }
 }
